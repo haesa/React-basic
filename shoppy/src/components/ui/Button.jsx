26c8f05605916a2092a70acff9533c68c05e138d
haesa <xdhaesa@gmail.com>
2023-07-01 17:06:31 +0900

    feat: Button UI 수정
    상품 업로드중일 땐 disabled

--- shoppy/src/components/ui/Button.jsx
+++ shoppy/src/components/ui/Button.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export default function Button({ text, onClick }) {
+export default function Button({ text, onClick, disabled }) {
   return (
     <button
       className='px-4 py-2 rounded-sm bg-brand text-white hover:brightness-110'
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
